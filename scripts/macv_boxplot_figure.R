50809d64579600173439f789e62a9d125eed277d
Claus Wilke <wilke@austin.utexas.edu>
2015-02-04 16:16:48 -0600

    figures.

--- scripts/macv_boxplot_figure.R
+++ scripts/macv_boxplot_figure.R
@@ -1,23 +1,35 @@
-library(plyr) # for ldply(), mapvalues()
-library(dplyr) # for select(), arrange(), group_by(), filter() 
+library(plyr) # for mapvalues()
+library(dplyr) # for join() 
 library(ggplot2) # ggplot()
-library(grid) 
-require(scales)
 
 #summary.I_sc = read.table("../data/macv_summary_data.csv", sep = ",", header = T)
-top.hits = read.table("../data/macv_top_hits.csv", sep = ",", header = T)
+top.hits <- read.table("../data/macv_top_hits.csv", sep = ",", header = T)
 
-             
+TfR1_names <- c("C. callosus", "Human", "Human L212V", "Mouse", "Mouse-Hum", "Rat", "Rat-Short", "Rat-Long")
+
+infection_data <- data.frame(TfR1 = TfR1_names, infects = factor(c("yes", "yes", "no", "no", "yes", "no", "partially", "yes")))
+
+top.hits$TfR1 <- mapvalues( top.hits$TfR1, from=c("C.cal_MACV", "human_MACV", "human.L212V_MACV", "mouse_MACV", "mouse.5aa_MACV", "rat_MACV", "rat.5aa_MACV", "rat.9aa_MACV"), to=TfR1_names)    
+
+top.hits$TfR1 <- factor( top.hits$TfR1, levels=c("Human", "Human L212V", "Mouse", "Mouse-Hum", "Rat", "Rat-Short", "Rat-Long", "C. callosus"))
+
+
+top.hits <- join(top.hits, infection_data)
+
 # Plot 1: box plots               
-p1 <- ggplot( top.hits, aes( x=TfR1, y=I_sc ) ) +
+p1 <- ggplot(top.hits, aes(x=TfR1, y=I_sc, color=infects)) +
         geom_boxplot() +
         ylab( "Interface score" ) +
-        theme_classic() #+ 
-        #coord_cartesian(ylim=c(-4.5, -10.5)) + 
-    	#scale_y_reverse(breaks=seq(-5, -10, -1)) 
+        theme_classic() +
+        scale_colour_manual(values = c("yes" = "forestgreen", "no" = "tomato3", "partially" = "tan2")) +
+        guides(color = guide_legend("MACV infects")) + 
+        theme(axis.title.x = element_text(face="bold"),
+              axis.text.x = element_text(face="bold"),
+              axis.title.y = element_text(face="bold"),
+              axis.text.y = element_text(face="bold"))
         
         
 # show plot
-#show(p2)
+show(p1)
 # save plot
-ggsave("../figures/macv_boxplot.pdf", plot = p1, width = 12, height = 8)
+ggsave("../figures/macv_boxplot.pdf", plot = p1, width = 9, height = 3, useDingbats=F)
--- scripts/macv_rmsd_figures.R
+++ scripts/macv_rmsd_figures.R
@@ -1,10 +1,22 @@
-suppressMessages(library(plyr)) # for ldply(), mapvalues()
-suppressMessages(library(dplyr)) # for select(), arrange(), group_by(), filter() 
-suppressMessages(library(ggplot2)) # ggplot()
+require(plyr) # for mapvalues()
+require(dplyr) # for filter() 
+require(ggplot2) # ggplot()
 
 macv.summary.I_sc = read.table("../data/macv_summary_data.csv", sep = ",", header = T)
-macv.top.hits = read.table("../data/macv_top_hits.csv", sep = ",", header = T)
 macv.data  = read.table("../data/macv_model_data.csv", sep = ",", header = T)
+
+TfR1_names <- c("C. callosus", "Human", "Human L212V", "Mouse", "Mouse-Hum", "Rat", "Rat-Short", "Rat-Long")
+
+infection_data <- data.frame(TfR1 = TfR1_names, infects = factor(c("yes", "yes", "no", "no", "yes", "no", "partially", "yes")))
+
+macv.data$TfR1 <- mapvalues( macv.data$TfR1, from=c("C.cal_MACV", "human_MACV", "human.L212V_MACV", "mouse_MACV", "mouse.5aa_MACV", "rat_MACV", "rat.5aa_MACV", "rat.9aa_MACV"), to=TfR1_names)    
+
+macv.data$TfR1 <- factor( macv.data$TfR1, levels=c("Human", "Human L212V", "Mouse", "Mouse-Hum", "Rat", "Rat-Short", "Rat-Long", "C. callosus"))
+
+macv.data <- join(macv.data, infection_data)
+
+macv.data.reduced <- filter(macv.data, TfR1 %in% c("Human", "Human L212V", "Mouse", "Mouse-Hum"))
+
 
 #Plot 1: Plot means 
 p1  = ggplot(macv.summary.I_sc, aes(x = -mean, y = sd.rms)) + 
@@ -19,8 +31,26 @@ p2 = ggplot()+
 	geom_point(macv.data, mapping = aes(x = rms, y = I_sc), size = 0.5)  +
 	xlim(0, 40) + #These are warnings because some points are outside this range
 	ylab("Interface score")+ 
-	xlab("RMS") + 
-	theme_classic() +
-	facet_wrap(~TfR1, ncol = 4) 
-ggsave("../figures/macv_funnels.pdf", plot = p2)
+	xlab("RMSD") + 
+	theme_bw() +
+	facet_wrap(~TfR1, ncol = 4) +
+	theme(axis.title.x = element_text(face="bold"),
+              axis.text.x = element_text(face="bold"),
+              axis.title.y = element_text(face="bold"),
+              axis.text.y = element_text(face="bold"))
+#show(p2) 
+ggsave("../figures/MACV_Funnels.pdf", plot = p2, width=9, height=4.5, useDingbats=F)
 
+p3 = ggplot()+
+	geom_point(macv.data.reduced, mapping = aes(x = rms, y = I_sc), size = 0.5)  +
+	xlim(0, 40) + #These are warnings because some points are outside this range
+	ylab("Interface score")+ 
+	xlab("RMSD") + 
+	theme_bw() +
+	facet_wrap(~TfR1, ncol = 4) +
+	theme(axis.title.x = element_text(face="bold"),
+              axis.text.x = element_text(face="bold"),
+              axis.title.y = element_text(face="bold"),
+              axis.text.y = element_text(face="bold"))
+show(p3) 
+ggsave("../figures/MACV_Funnels_reduced.pdf", plot = p3, width=9, height=2.5, useDingbats=F)
